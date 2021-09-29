c69c5250c3ce60d7479ca61425e8efb132bcd1f6
Phan Long Hieu <hieuflong@gmail.com>
2021-09-29 14:17:21 +0700

    Add router for app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import { CssBaseline } from "@mui/material"
 import React from "react"
+import { Route, Switch } from "react-router-dom"
 import { SideMenu } from "./components"
+import Cryptocurrencies from "./pages/crypto.page"
+import Exchange from "./pages/exchange.page"
+import Home from "./pages/home.page"
+import New from "./pages/news.page"
+import "./App.css"
 
 const App = () => {
   return (
     <>
       <CssBaseline></CssBaseline>
-      <SideMenu></SideMenu>
+      <div className="app">
+        <div className="navbar">
+          <SideMenu></SideMenu>
+        </div>
+        <div className="main"></div>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route exact path="/exchanges">
+            <Exchange></Exchange>
+          </Route>
+          <Route exact path="/cryptocurrencies">
+            <Cryptocurrencies></Cryptocurrencies>
+          </Route>
+          <Route exact path="/news">
+            <New></New>
+          </Route>
+        </Switch>
+      </div>
     </>
   )
 }
---
+++ src/pages/exchange.page.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Exchange = () => {
+  return <div>Exchange Page</div>
+}
+
+export default Exchange
